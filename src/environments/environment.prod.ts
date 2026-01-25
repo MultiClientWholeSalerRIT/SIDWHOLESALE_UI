849fe6e2c3649c8a1db02b9c43c5cbc273c0ce0b
Sayeed <61646110+shaikmohammedsayeed2@users.noreply.github.com>
2026-01-25 22:19:01 +0530

    Update production API URL in environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:'https://sid-d5c3hkgcgma7e6h9.centralus-01.azurewebsites.net/api/'
+  // apiUrl:'https://sid-d5c3hkgcgma7e6h9.centralus-01.azurewebsites.net/api/'
+  apiUrl:'https://multiclient-ggdgfgcqcdexhgca.southindia-01.azurewebsites.net/api/'
 };
